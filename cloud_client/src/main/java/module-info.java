694e8d0c1d5e1cb15cd0a79fe3756aa88e24926b
IlyaM <ilya.mostolygin@gmail.com>
2022-03-31 19:11:39 +0300

    cloud storage

---
+++ cloud_client/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.cloudstorage {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires io.netty.transport;
+    requires lombok;
+    requires org.slf4j;
+    requires io.netty.codec;
+    requires java.desktop;
+
+
+    opens com.example.cloudstorage.client to javafx.fxml;
+    exports com.example.cloudstorage.client;
+}
---
+++ cloud_server/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.cloudstorage {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires io.netty.transport;
+    requires lombok;
+    requires org.slf4j;
+    requires io.netty.codec;
+    requires java.desktop;
+}
